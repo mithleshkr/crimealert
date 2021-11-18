ea00fa8d08bba54d43b1cffdfd6ea25e7385c1ec
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-11-18 09:24:05 +0530

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import {TextField, Button} from '@material-ui/core';
+
+function Login() {
+    return (
+        <div>
+            
+            <TextField  placeholder="Enter username" label="Username"/><br />
+            <TextField  placeholder="Enter password" label="Password"/><br/><br />
+            <Button variant="contained">Login</Button>
+            <p>New User?<span></span></p>
+            
+            
+        </div>
+    )
+}
+
+export default Login
 
