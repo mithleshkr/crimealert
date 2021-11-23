4c0a45938e95c723c9262d49b84536021b199825
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-11-23 15:36:59 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,101 +1,45 @@
-import React,{useState} from 'react';
-import { Avatar, Button, Grid ,Paper, TextField, Typography, Link} from '@material-ui/core';
-import LockRoundedIcon from '@material-ui/icons/LockRounded';
+import React from 'react'
+import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import { Formik,Form, Field, ErrorMessage} from 'formik';
-import * as Yup from 'yup';
-// import { useHistory } from "react-router-dom";
-//import './Login.css'
+const Login=({handleChange})=>{
 
-
-const Login = () => {
-    //const history=useHistory();
-    const paperstyle={padding:20,height:'60vh',width:280,margin:"20px auto"}
-    const avatarstyle={backgroundColor:"blue"}
+    const paperStyle={padding :20,height:'73vh',width:300, margin:"0 auto"}
+    const avatarStyle={backgroundColor:'#1bbd7e'}
     const btnstyle={margin:'8px 0'}
-    const initialValues={
-        username:"",
-        password:"",
-        remember:false
-    }
-    const validationSchema=Yup.object().shape({
-        username:Yup.string().email('Please enter valid username').required("Required"),
-        password:Yup.string().required("Required")
-    })
-    const onSubmit=(values,props)=>{
-        props.resetForm()
-        
-        console.log(values)
-        console.log(props)
-    }
-    const[password, setPassword]=useState("");
-    const[usernm, setUsernm]=useState("");
-
-
     return(
-        <div style={{background:"grey"}}>
-            <Grid style={{display:"flex",justifyContent:"center",flex:1,height:'100vh'}}>
-                <Paper elvation={10} style={paperstyle}>
-                    <Grid align='center'>
-                        <Avatar style={avatarstyle}><LockRoundedIcon /></Avatar>
-                        <h2>Login</h2>
-                    </Grid>
-                    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                        {(props)=>(
-                            <Form>
-                                
-                    <Field as={TextField} 
-                    helperText={<ErrorMessage name="username" style={{color:"red"}}/>}
-                    label="Username"
-                    name="username"
-                    placeholder="Enter username"
-                    value={usernm}
-                    onChange={(e)=>setUsernm(e.target.value)}
-                    fullWidth 
-                    required />
-                    <Field as={TextField}
-                    helperText={<ErrorMessage name="password"/>}
-                    label="Password" 
-                    name="password"
-                    placeholder="Enter password" 
-                    type="password" 
-                    value={password}
-            onChange={(e)=>setPassword(e.target.value)}
-                    fullWidth 
-                    required />
-                    <Field as={FormControlLabel}
-                    name="remember"
+        <Grid>
+            <Paper  style={paperStyle}>
+                <Grid align='center'>
+                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <FormControlLabel
                     control={
                     <Checkbox
-                    name="checkedB"
-                    color="primary"
-                     />
+                        name="checkedB"
+                        color="primary"
+                    />
                     }
                     label="Remember me"
-                    />
-                    <Button 
-                    // onClick={() => history.push("Dashboard")}
-                    style={btnstyle}
-                    type="submit"
-                    color="primary"
-                    variant="contained"
-                    disabled={!password + !usernm}
-                    fullWidth>
-                    LOGIN
-                    </Button>
-                            </Form>
-                        )}
-                    </Formik>
-                    <Typography> New user?
-                        <Link href="signup">
-                            Click Here
-                        </Link>
-                    </Typography>
-                </Paper>
-            </Grid>
-        </div>
-
+                 />
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                <Typography >
+                     <Link href="#" >
+                        Forgot password ?
+                </Link>
+                </Typography>
+                <Typography > Do you have an account ?
+                     <Link href="#" onClick={()=>handleChange("event",1)} >
+                        Sign Up 
+                </Link>
+                </Typography>
+            </Paper>
+        </Grid>
     )
 }
-export default Login;
+
+export default Login
