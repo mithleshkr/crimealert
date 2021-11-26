51eed947a272aec599e3cdbebdac4393a3bd2d16
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2021-11-26 10:51:38 +0530

    Update Postform.js

--- Postform.js
+++ Postform.js
@@ -7,10 +7,13 @@ function Postform() {
     const [crimedate, setCrimedate] = useState("");
     const [crimetime, setCrimetime] = useState("");
     const [location, setLocation] = useState("");
-
-    function save() {
-        console.warn({ about, crimedate, crimetime, location });
-        let data={ about, crimedate, crimetime, location }
+    const [cimage, setCimage] = useState(null);
+    function save(e) {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append(cimage);
+        console.warn({ about, crimedate, crimetime, location, cimage});
+        let data={ about, crimedate, crimetime, location,cimage}
         fetch("http://localhost:3333/post",{
             method:'POST',
             headers:{
@@ -24,13 +27,36 @@ function Postform() {
     
          })
       }
+
+      const handleImage = (e)=>{
+          setCimage(URL.createObjectURL(e.target.files[0]));
+      }
+    //   function imgPick(e)
+    //   {
+    //     setCimage(e.target.files[0]);
+    //     console.log("22222222");
+    //     console.log(cimage)
+    //   }
+
+    //    function handleChange (e) 
+    //    {
+           
+    //      if(e.target.files.length)
+    //      {
+    //          setCimage({preview:URL.createObjectURL(e.target.files[0])});
+    //      }
+    //    }
+    
     return (
         <div >
             <form style={{display:"flex",flexDirection:"column"}}>
                 <TextField label="About Post" placeholder="Write about the post" value={about} onChange={(e)=>{setAbout(e.target.value)}} />
                 <TextField type="date" value={crimedate} onChange={(e)=>{setCrimedate(e.target.value)}} />
                 <TextField type="time" value={crimetime} onChange={(e)=>{setCrimetime(e.target.value)}} />
-                <TextField label="location" placeholder="Enter crime location" value={location} onChange={(e)=>{setLocation(e.target.value)}} /><br/>
+                <TextField label="location" placeholder="Enter crime location" value={location} onChange={(e)=>{setLocation(e.target.value)}} />
+                <input   onChange={handleImage} type="file"  />
+                
+                <br/>
                 <Button variant="contained" size="small" color="primary" onClick={save}>Add</Button>
             </form>
             
